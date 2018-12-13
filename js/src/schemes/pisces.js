43e68c166e808aeed18e3de83c0c6c7f69b73e47
jun <13237124836@163.com>
2018-12-13 20:47:37 +0800

    Site updated: 2018-12-13 20:47:36

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Sun Dec 02 2018 20:09:51 GMT+0800 (China Standard Time)
+// build time:Thu Dec 13 2018 20:44:09 GMT+0800 (China Standard Time)
 $(document).ready(function(){var i=$(".sidebar-inner");var t=CONFIG.sidebar.offset?CONFIG.sidebar.offset:12;function e(){return $(".header-inner").height()+t}function r(){var i=$(".footer-inner");var t=i.outerHeight(true)-i.outerHeight();var e=i.outerHeight(true)+t;return e}function a(){var a=e();var f=r();var n=$("#sidebar").height()+NexT.utils.getSidebarb2tHeight();var o=$("#content").height();if(a+n<o){i.affix({offset:{top:a-t,bottom:f}});i.affix("checkPosition")}$("#sidebar").css({"margin-top":a,"margin-left":"initial"})}function f(){$(window).off(".affix");i.removeData("bs.affix").removeClass("affix affix-top affix-bottom");a()}function n(){var i=window.matchMedia("(min-width: 992px)");i.addListener(function(i){if(i.matches){f()}})}a();n()});
 //rebuild by neat 
